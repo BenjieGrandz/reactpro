ec474340bfdf923427811a7f843c66cbbc0c8f79
BenjieGrandz <benkoimet@gmail.com>
2024-10-28 17:33:14 +0300

    I just added a number of fruits and their data and displayed it

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 export default function Fruits() {
-  const fruits = ["Bananas", "Mangoes", "Oranges", "Apples", "Thorn Melons"];
+  //const fruits = ["Bananas", "Mangoes", "Oranges", "Apples", "Thorn Melons"];
+  const fruits = [
+    { name: "Bananas", price: "10", emoji: "🍌" },
+    { name: "Mangoes", price: "2", emoji: "🥭" },
+    { name: "Oranges", price: "13", emoji: "🍈" },
+    { name: "Apples", price: "42", emoji: "🍎" },
+    { name: "Melons", price: "250", emoji: "🍉" },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.name}>
+            {fruit.emoji}- {fruit.name} $ {fruit.price}
+          </li>
         ))}
       </ul>
     </div>
